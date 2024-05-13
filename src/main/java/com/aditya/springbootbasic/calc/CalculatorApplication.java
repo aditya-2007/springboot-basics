b5b8d5fbec2326ed8760e85eabc7010d74ca0e96
aditya.bhogale@signanthealth.com <aditya.bhogale@signanthealth.com>
2024-05-13 13:08:31 +0530

    removed manual retrieval of bean from appcontext and instead added apprunner to seed data

--- src/main/java/com/aditya/springbootbasic/calc/CalculatorApplication.java
+++ src/main/java/com/aditya/springbootbasic/calc/CalculatorApplication.java
@@ -1,7 +1,6 @@
 package com.aditya.springbootbasic.calc;
 
-import java.util.List;
-
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,15 +10,37 @@ public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		var ctx = SpringApplication.run(CalculatorApplication.class, args);
+		
+		//better not to retrieve beans manually. Use DI instead.
+		/*
 		var calculator= ctx.getBean(Calculator.class);
 		calculator.calculate(10, 20, "+");
 		calculator.calculate(10, 23, "*");
 		calculator.calculate(78, 44, "+");
 		calculator.calculate(1, 0, "*");
+		*/
 	}
 	
+//	@Bean
+//	Calculator calculator(List<Operation> operations) {
+//		return new Calculator(operations);
+//	}
+	
 	@Bean
-	Calculator calculator(List<Operation> operations) {
-		return new Calculator(operations);
+	ApplicationRunner calculationRunner(Calculator calculator) {
+		
+		return args -> {
+			
+			calculator.calculate(10, 20, "+");
+			calculator.calculate(10, 23, "*");
+			calculator.calculate(78, 44, "+");
+			calculator.calculate(1, 0, "*");
+		};
 	}
 }
+
+
+
+
+
+
